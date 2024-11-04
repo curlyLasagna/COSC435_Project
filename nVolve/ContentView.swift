028ce2d17a1c75450d47bf9898865224296f7c13
Rasheed Nolley <nolleyrasheed@gmail.com>
2024-11-04 18:58:17 -0500

    init commit; got a map

--- nVolve/ContentView.swift
+++ nVolve/ContentView.swift
@@ -1,21 +1,30 @@
 //
 //  ContentView.swift
-//  nVolve
+//  mapkit
 //
-//  Created by Luis on 10/10/24.
+//  Created by Rasheed Nolley on 10/28/24.
 //
 
 import SwiftUI
-
+import MapKit
 struct ContentView: View {
+    @State private var position : MapCameraPosition = .automatic
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("Filters")
+                Spacer()
+                Image(systemName: "slider.horizontal.3")
+            }.padding().overlay(
+              Rectangle()
+                .stroke(Color.black, lineWidth: 1)
+            )
+            Map(position: $position) {
+                Marker("Towson University", coordinate: CLLocationCoordinate2D(latitude: 39.3937,longitude: -76.6082))
+            }.mapStyle(.standard).frame(height: 400)
+            Spacer()
         }
-        .padding()
     }
 }
 
---
+++ nVolve/mapkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapkitApp.swift
+//  mapkit
+//
+//  Created by Rasheed Nolley on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct mapkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
