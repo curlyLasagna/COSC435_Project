8eef24a5ad4c473908c6084498ef7f560a3572b7
Luis Dale <luis.gcodes@gmail.com>
2024-11-15 14:02:14 -0500

    created model

--- nVolve/Model/InvolvedModel.swift
+++ nVolve/Model/InvolvedModel.swift
@@ -7,21 +7,33 @@
 
 import SwiftUICore
 
-struct Involved: Codable, Identifiable {
+struct InvolvedEvents: Codable, Identifiable {
     let id: String
     let orgName: String
-    let orgPhoto: Image
+    let orgPhoto: String
     let eventName: String
     let eventDescription: String
     let eventLocation: String
     let startDate: Date
     let endDate: Date
-    let imagePath: Image
+    let imagePath: String
     let eventTheme: String
     let latitude: String
     let longitude: String
+    let perks: [String]
     
     enum MyCodingKeys: String, CodingKey {
         case id = "id"
-        
+        case orgName = "organizationName"
+        case orgPhoto = "organizationProfilePicture"
+        case eventName = "name"
+        case eventDescription = "description"
+        case eventLocation = "location"
+        case startDate = "startsOn"
+        case endDate = "endsOn"
+        case imagePath = "imagePath"
+        case eventTheme = "theme"
+        case latitude = "latitude"
+        case longitude = "longitude"
+        case perks = "benefitNames"
     }}
--- nVolve/ViewModel/EventViewModel.swift
+++ nVolve/ViewModel/EventViewModel.swift
@@ -7,4 +7,9 @@
 
 import UIKit
 
-
+@Observable class EventViewModel {
+    var events: [InvolvedEvents] = []
+    func fetchData() {
+        guard let endpoint = URL()
+    }
+}
