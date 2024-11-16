a67d68509b8ba545d13d2c7dea8fd15ad79bea1d
Romerico <romedavid2@outlook.com>
2024-11-15 19:47:31 -0500

    Made this PR match with dev branch

---
+++ nVolve/nVolveApp.swift
@@ -0,0 +1,17 @@
+//
+//  nVolveApp.swift
+//  nVolve
+//
+//  Created by Luis on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct nVolveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
