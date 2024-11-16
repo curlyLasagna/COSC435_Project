bf53d0284ae26724d06703cc1ebc50f00b570ee6
Luis Dale <luis.gcodes@gmail.com>
2024-11-16 10:13:39 -0500

    call api with current date

--- nVolve/ViewModel/EventViewModel.swift
+++ nVolve/ViewModel/EventViewModel.swift
@@ -5,20 +5,35 @@
 //  Created by Luis on 11/12/24.
 //
 
-import UIKit
 import Alamofire
+import UIKit
 
 @Observable class EventViewModel {
     var events: [InvolvedEvents] = []
+    // ':' is encoded as %3
+    // 'T' is the separator between time and date
     func fetchTodayEvents() {
-        AF.request()
+        let encodedToday = ISO8601DateFormatter().string(from: Date())
+            .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let endpoint =
+            "https://involved.towson.edu/api/discovery/event/search?startsAfter=\(encodedToday)"
+        AF.request(endpoint).responseDecodable(of: [InvolvedEvents].self) {
+            response in
+            switch response.result {
+            case .success(let data):
+                print(data)
+            case .failure(let err):
+                print(err)
+
+            }
+        }
     }
-    
+
     func fetchEventsByPerks() {
-        
+
     }
-    
+
     func fetchEventsByTheme() {
-        
+
     }
 }
