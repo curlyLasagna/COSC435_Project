0ac1a75e05152a48d828628e45f9d40efab66d43
Romerico <romedavid2@outlook.com>
2024-12-05 06:26:38 -0500

    Woopsies, a lot of events weren't loading

--- nVolve/Features/Content/ContentViewModel.swift
+++ nVolve/Features/Content/ContentViewModel.swift
@@ -16,44 +16,46 @@ class ContentViewModel: ObservableObject {
     @Published var events: [InvolvedEvent] = []
 
     func fetchTodayEvents() {
-        let isoFormatter = ISO8601DateFormatter()
-        isoFormatter.formatOptions = [.withInternetDateTime]
+        // This function is only for involved @ TU. A refactor will be required to accodomate other data sources
+        let encodedStart = ISO8601DateFormatter().string(from: Date())
+            .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
 
-        guard let encodedStart = isoFormatter.string(from: Date())
-                .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-              let encodedEnd = isoFormatter.string(from: Date().addingTimeInterval(86400))
-                .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-        else {
-            return
-        }
+        let encodedEnd = ISO8601DateFormatter().string(
+            from: Date().advanced(by: 86400)
+        ).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
 
-        let endpoint = "https://involved.towson.edu/api/discovery/event/search?startsAfter=\(encodedStart)&endsBefore=\(encodedEnd)"
-
-        AF.request(endpoint).responseDecodable(of: InvolvedEvents.self) { response in
+        let endpoint =
+            "https://involved.towson.edu/api/discovery/event/search? startsAfter=\(encodedStart!)endsBefore=\(encodedEnd!)"
+        AF.request(endpoint).responseDecodable(of: InvolvedEvents.self) {
+            response in
             switch response.result {
             case .success(let data):
-                DispatchQueue.main.async {
-                    self.events = data.value
-                }
+                self.events = data.value
             case .failure(let err):
-                print("ContentViewModel: Error fetching events - \(err)")
+                print(err)
             }
         }
     }
 
-    func getCoordinates(latitude: String?, longitude: String?) -> CLLocationCoordinate2D {
+    func getCoordinates(latitude: String?, longitude: String?)
+        -> CLLocationCoordinate2D
+    {
         if let latitude = Double(latitude ?? "39.394839"),
-           let longitude = Double(longitude ?? "-76.610880") {
-            return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let longitude = Double(longitude ?? "-76.610880")
+        {
+            return CLLocationCoordinate2D(
+                latitude: latitude, longitude: longitude)
         }
         // Default to the Union
-        return CLLocationCoordinate2D(latitude: 39.394839, longitude: -76.610880)
+        return CLLocationCoordinate2D(
+            latitude: 39.394839, longitude: -76.610880)
     }
 
     func getImages(imgPath: String?) -> String {
         // To get the full image path, prepend the returned image path with https://se-images.campuslabs.com/clink/images/
         if let fullImgPath = imgPath {
-            return "https://se-images.campuslabs.com/clink/images/" + fullImgPath
+            return "https://se-images.campuslabs.com/clink/images/"
+                + fullImgPath
         }
         return ""
     }
@@ -62,7 +64,8 @@ class ContentViewModel: ObservableObject {
         let isoFormatter = ISO8601DateFormatter()
         isoFormatter.formatOptions = [.withInternetDateTime]
         guard let dateAsString = dateAsString,
-              let createdDate = isoFormatter.date(from: dateAsString) else {
+            let createdDate = isoFormatter.date(from: dateAsString)
+        else {
             return "No date"
         }
 
@@ -76,7 +79,8 @@ class ContentViewModel: ObservableObject {
         guard var result = text else { return "" }
 
         // Remove HTML tags using a regex
-        result = result.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression)
+        result = result.replacingOccurrences(
+            of: "<[^>]+>", with: "", options: .regularExpression)
 
         // Decode and remove HTML entities like &nbsp;, &amp; into plain text equivalents
         if let decodedData = result.data(using: .utf8) {
@@ -84,7 +88,7 @@ class ContentViewModel: ObservableObject {
                 data: decodedData,
                 options: [
                     .documentType: NSAttributedString.DocumentType.html,
-                    .characterEncoding: String.Encoding.utf8.rawValue
+                    .characterEncoding: String.Encoding.utf8.rawValue,
                 ],
                 documentAttributes: nil
             )
